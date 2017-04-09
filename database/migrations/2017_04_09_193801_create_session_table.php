5efe4e41de297bf11be938578d343ac488c9ed5b
woodyj888 <woodyj888@googlemail.com>
2017-04-09 21:50:55 +0100

    Add migrations: Create session management tables.

---
+++ database/migrations/2017_04_09_193801_create_session_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSessionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('session', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('session_id')->unique();
+            $table->ipAddress('ip_address');
+            $table->macAddress('mac_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('session');
+    }
+}
