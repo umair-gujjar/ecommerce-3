1852b3e5abcee21ecc08f159321139b440638f1a
woodyj888 <woodyj888@googlemail.com>
2017-04-12 12:03:48 +0100

    Add order models.

---
+++ app/Models/OrderLine.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
+
+class OrderLine extends Model
+{
+    protected $table = 'order_line';
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
