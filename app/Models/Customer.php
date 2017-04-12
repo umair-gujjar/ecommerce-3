72d962752c00be74556d2827ba4ac701d5e48b52
woodyj888 <woodyj888@googlemail.com>
2017-04-12 13:14:58 +0100

    Add Eloquent addresses() foreign key relationship to Customer model.

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Address;
 
 class Customer extends Model
 {
@@ -15,5 +16,13 @@ class Customer extends Model
     public function user()
     {
     	return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the address models owned by this customer.
+     */
+    public function addresses()
+    {
+    	return $this->hasMany(Address::class);
     }
 }
